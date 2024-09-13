4fda8f6fdaf86964feb3d7997a75498b2ec53ffc
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2024-09-13 13:07:41 +0530

    code update to stay compliant with fastai v2

--- libpy/weapon.py
+++ libpy/weapon.py
@@ -1,54 +1,50 @@
+import os
 import sys
 import glob
-from fastai.vision.image import open_image
-from fastai.basic_train import load_learner
-from fastai.vision.transform import get_transforms
-from fastai.vision.data import ImageDataBunch, imagenet_stats
+from fastai.vision.all import *
 
-
-def prep_model(path: str):
-    model_path = './model/'
-    classes = ('knife', 'handgun', 'rifle')
-
-    data = ImageDataBunch.single_from_classes(
-        path, classes, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
+def load_model(path: str):
+    cwd = os.getcwd()
+    model_path = cwd + "/" + "model/awd.pkl"
     return load_learner(model_path)
 
 
-def weapon_check(path_list: list, model):
+def weapon_check(path_list: list, model, out):
     for path in path_list:
-        detect(path, model)
+        print(path)
+        detect(path, model, out)
 
 
-def detect(img_path: str, model):
-    img = open_image(img_path)
+def detect(img_path: str, model, out):
+    img = load_image(img_path)
     pred_class, pred_idx, outputs = model.predict(img)
 
     if (outputs[0] < 0.7) and (outputs[1] < 0.7) and (outputs[2] < 0.7):
-        print('#######################################################################')
-        print('File: ', img_path.split('/')[-1])
-        print('kai na malyu')
-        print()
+        print("#######################################################################", file=out)
+        print("File: ", img_path.split("/")[-1], file=out)
+        print("no weapon found", file=out)
+        print("", file=out)
     else:
-        print('#######################################################################')
-        print('File: ' + img_path.split('/')[-1])
-        print('prediction: ' + pred_class)
-        print('probabilities:')
-        print('handgun: ' + str(outputs[0]) + '\nknife: ' +
-              str(outputs[1]) + '\nrifle: ' + str(outputs[2]))
+        print("#######################################################################", file=out)
+        print("File: " + img_path.split("/")[-1], file=out)
+        print("prediction: " + pred_class, file=out)
+        print("probabilities:", file=out)
+        print("handgun: " + str(outputs[0]) + "\nknife: " + str(outputs[1]) + "\nrifle: " + str(outputs[2]), file=out)
 
 
 def main():
-    outfile = open('./weapon_rec_result.txt', 'w')
-    path = sys.argv[1]
+    cwd = os.getcwd()
+    outpath = cwd + "/weapon_rec_result.txt"
+    outfile = open(outpath, "w")
 
-    if path == "":
+    if len(sys.argv) < 2:
         print("ERROR: Path cannot be empty", file=outfile)
         return
 
-    model = prep_model(path)
+    path = sys.argv[1]
+    model = load_model(path)
     path_list = glob.glob(path+"*")
-    weapon_check(path_list, model)
+    weapon_check(path_list, model, outfile)
 
 
 if __name__ == "__main__":
